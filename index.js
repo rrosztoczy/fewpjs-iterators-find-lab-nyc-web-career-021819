027872460dcfa0db6b676c1b9146eca928810236
rrosztoczy <rrosztoczy@gmail.com>
2019-04-10 15:15:54 -0400

    done

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const testVar = {}
 
+
+
+// superbowlWin
+// 1) returns a year the KC Chiefs won the superbowl
+// 2) returns undefined if the record has no win objects
+
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  const winYear = array.find(year => year.result === "W")
+  return winYear ? winYear.year : undefined
+}
+
